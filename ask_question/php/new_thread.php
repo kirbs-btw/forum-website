71e613aa49077534b9b2f290cb2150faf7dad534
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-12-16 15:00:39 +0100

    Add files via upload

--- ask_question/php/new_thread.php
+++ ask_question/php/new_thread.php
@@ -23,9 +23,26 @@
   if (mysqli_connect_error()){
     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
   } else{
-    $sql = "INSERT INTO feedback VALUES('$topic' , '$content', DATE '$today', '$time');";
+    $sql = "INSERT INTO thread VALUES('$id' ,'$topic' , '$content', DATE '$today', '$time');";
     mysqli_query($conn, $sql);
   }
+
+  //threadTopic conversion
+  $threadTopic = "";
+
+  for ($i=0; $i < len($topic); $i++) {
+    if ($topic[i] != " ") {
+      $threadTopic = $threadTopic + $topic[i]
+    }else {
+      $threadTopic = $threadTopic + "_"
+    }
+  }
+  
+
+  // create file
+  $file = fopen("../../thread/" . $threadTopic)
+
+
 
   // redirect
   header("Location:../ask.html");
